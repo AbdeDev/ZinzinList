61d6d328573ecc88b975dc775ee2ec0747b8e0f0
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2025-03-10 04:08:43 +0100

    retry to build in prod again

--- zinzin-list/vite.config.ts
+++ zinzin-list/vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import path from "node:path";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src", // Plus besoin de path.resolve(__dirname, "./src")
     },
   },
 });
